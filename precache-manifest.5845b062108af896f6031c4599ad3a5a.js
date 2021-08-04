b622fdc144b52b10ed55f42c1935aaa6de7c7923
R-o-t-I <rulers.oftheinfinite@mail.ru>
2021-08-05 02:53:50 +0300

    Updates

---
+++ precache-manifest.5845b062108af896f6031c4599ad3a5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ae485face83f17098e4",
+    "url": "/article/static/css/main.8139a136.chunk.css"
+  },
+  {
+    "revision": "0ae485face83f17098e4",
+    "url": "/article/static/js/main.a540b1fa.chunk.js"
+  },
+  {
+    "revision": "e60f95fdec0a3c38af07",
+    "url": "/article/static/js/runtime~main.77401486.js"
+  },
+  {
+    "revision": "3e93ef7b47b871fd6fc2",
+    "url": "/article/static/css/2.41ffc886.chunk.css"
+  },
+  {
+    "revision": "3e93ef7b47b871fd6fc2",
+    "url": "/article/static/js/2.62c3b51e.chunk.js"
+  },
+  {
+    "revision": "0221da9d4f16c519d8d8",
+    "url": "/article/static/js/3.0569262b.chunk.js"
+  },
+  {
+    "revision": "370ac7ca980d14a70d80",
+    "url": "/article/static/js/4.7ed40279.chunk.js"
+  },
+  {
+    "revision": "369f7bea2646cfbe113630e55c56a75d",
+    "url": "/article/index.html"
+  }
+];
